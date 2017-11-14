103f320634addb4e66de75d5d6d743366d64c40f
Santa <santazaaa@gmail.com>
2017-11-14 12:38:49 +0700

    Add playfab sdk

--- server.js
+++ server.js
@@ -4,11 +4,18 @@
 var net = require('net');
 var messageHandler = require('./messageHandler');
 
+// PlayFab setup
+var PlayFab = require("playfab-sdk/Scripts/PlayFab/PlayFab");
+var PlayFabServer = require("playfab-sdk/Scripts/PlayFab/PlayFabServer");
+PlayFab.settings.titleId = process.env.PLAYFAB_API_TITLE;
+PlayFab.settings.developerSecretKey = process.env.PLAYFAB_SECRET_KEY;
+console.log("PlayFab settings: " + JSON.stringify(PlayFab.settings));
+
 // Start a TCP Server
 net.createServer(function (socket) {
 
   messageHandler.onConnected(socket);
-
+  
   // Handle incoming messages from clients.
   socket.on('data', function (data) {
     messageHandler.parseRawData(data);
